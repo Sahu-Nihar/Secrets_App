e22362d3a8de923cb312dd3d7b04e65dd1472de4
Roronoa Zoro <sahu666necromancer@gmail.com>
2020-07-16 01:17:56 +0530

    Authentication project- step 5: used salting in hash to further increase the security

--- app.js
+++ app.js
@@ -3,7 +3,8 @@ const express = require("express");
 const bodyParser = require("body-parser");
 const ejs = require("ejs");
 const mongoose = require("mongoose");
-const md5 = require("md5");
+const bcrypt = require("bcrypt");
+const saltRounds = 10;
 
 const app = express();
 
@@ -53,23 +54,27 @@ app.get("/register", (req, res) => {
 
 app.post("/register", (req, res) => {
 
-    const new_user = new User({
-        email: req.body.username,
-        password: md5(req.body.password)
+    bcrypt.hash(req.body.password, saltRounds, (err, hash) => {
+
+        const new_user = new User({
+            email: req.body.username,
+            password: hash
+        });
+
+        new_user.save((err) => {
+            if (err) {
+                console.log(err);
+            } else {
+                res.render("secrets");
+            }
+        });
     });
 
-    new_user.save((err) => {
-        if (err) {
-            console.log(err);
-        } else {
-            res.render("secrets");
-        }
-    });
 });
 
 app.post("/login", (req, res) => {
     const username = req.body.username;
-    const password = md5(req.body.password);
+    const password = req.body.password;
 
     User.findOne({
         email: username
@@ -79,9 +84,12 @@ app.post("/login", (req, res) => {
             console.log(err);
         } else {
             if (found_user) {
-                if (found_user.password === password) {
-                    res.render("secrets");
-                }
+                bcrypt.compare(password, found_user.password, (err, result) => {
+
+                    if (result === true) {
+                        res.render("secrets");
+                    }
+                });
             }
         }
     });
